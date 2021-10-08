ff9c8330b9b78d35a0c77936d79299607e54f7fb
Abdulrafay <54246840+abdulrafayn001@users.noreply.github.com>
2021-10-08 23:35:15 +0500

    Add moving cards for feedback in main page

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import SlideShow from './SlideShow'
+import Feedback from './feedback/Feedback'
 export default function Main() {
     return (
         <div>
             <SlideShow/>
+            <Feedback/>
         </div>
     )
 }
