deede462a73465c799b40fc3361db025de274293
Abdulrafay <54246840+abdulrafayn001@users.noreply.github.com>
2021-10-12 17:31:03 +0500

    Add footer to the main page

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -3,6 +3,7 @@ import SlideShow from './SlideShow'
 import Feedback from './feedback/Feedback'
 import Categories from './categories/Categories'
 import Card from './Card'
+import Footer from './Footer'
 export default function Main() {
     return (
         <div>
@@ -16,8 +17,8 @@ export default function Main() {
                 <Card image={"https://www.freecodecamp.org/news/content/images/size/w2000/2020/12/fcc-bg-image-2.png" } name={"Canvas Painting"} price={1200} rating={4.5}/>
                 <Card image={"https://www.freecodecamp.org/news/content/images/size/w2000/2020/12/fcc-bg-image-2.png" } name={"Canvas Painting"} price={1200} rating={4.5}/>
                 <Card image={"https://www.freecodecamp.org/news/content/images/size/w2000/2020/12/fcc-bg-image-2.png" } name={"Canvas Painting"} price={1200} rating={4.5}/>
-
             </div>
+            <Footer/>
         </div>
     )
 }
