6eaa7b805ce1918ba61c69fc1ca254362cd71569
Abdulrafay <54246840+abdulrafayn001@users.noreply.github.com>
2022-01-18 18:39:11 +0500

    Footer file location changed

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,24 +1,66 @@
-import React from 'react'
-import SlideShow from './SlideShow'
-import Feedback from './feedback/Feedback'
-import Categories from './categories/Categories'
-import Card from './Card'
-import Footer from './Footer'
+import React from "react";
+import SlideShow from "./SlideShow";
+import Feedback from "./feedback/Feedback";
+import Categories from "./categories/Categories";
+import Card from "./Card";
+import Footer from "../footer/Footer";
 export default function Main() {
-    return (
-        <div>
-            <SlideShow/>
-            <Categories/>
-            <Feedback/>
-            <div className="flex justify-evenly flex-wrap">
-                <Card image={"https://www.freecodecamp.org/news/content/images/size/w2000/2020/12/fcc-bg-image-2.png" } name={"Canvas Painting"} price={1200} rating={4.5}/>
-                <Card image={"https://www.freecodecamp.org/news/content/images/size/w2000/2020/12/fcc-bg-image-2.png" } name={"Canvas Painting"} price={1200} rating={4.5}/>
-                <Card image={"https://www.freecodecamp.org/news/content/images/size/w2000/2020/12/fcc-bg-image-2.png" } name={"Canvas Painting"} price={1200} rating={4.5}/>
-                <Card image={"https://www.freecodecamp.org/news/content/images/size/w2000/2020/12/fcc-bg-image-2.png" } name={"Canvas Painting"} price={1200} rating={4.5}/>
-                <Card image={"https://www.freecodecamp.org/news/content/images/size/w2000/2020/12/fcc-bg-image-2.png" } name={"Canvas Painting"} price={1200} rating={4.5}/>
-                <Card image={"https://www.freecodecamp.org/news/content/images/size/w2000/2020/12/fcc-bg-image-2.png" } name={"Canvas Painting"} price={1200} rating={4.5}/>
-            </div>
-            <Footer/>
-        </div>
-    )
+  return (
+    <div>
+      <SlideShow />
+      <Categories />
+      <Feedback />
+      <div className="flex justify-evenly flex-wrap">
+        <Card
+          image={
+            "https://www.freecodecamp.org/news/content/images/size/w2000/2020/12/fcc-bg-image-2.png"
+          }
+          name={"Canvas Painting"}
+          price={1200}
+          rating={4.5}
+        />
+        <Card
+          image={
+            "https://www.freecodecamp.org/news/content/images/size/w2000/2020/12/fcc-bg-image-2.png"
+          }
+          name={"Canvas Painting"}
+          price={1200}
+          rating={4.5}
+        />
+        <Card
+          image={
+            "https://www.freecodecamp.org/news/content/images/size/w2000/2020/12/fcc-bg-image-2.png"
+          }
+          name={"Canvas Painting"}
+          price={1200}
+          rating={4.5}
+        />
+        <Card
+          image={
+            "https://www.freecodecamp.org/news/content/images/size/w2000/2020/12/fcc-bg-image-2.png"
+          }
+          name={"Canvas Painting"}
+          price={1200}
+          rating={4.5}
+        />
+        <Card
+          image={
+            "https://www.freecodecamp.org/news/content/images/size/w2000/2020/12/fcc-bg-image-2.png"
+          }
+          name={"Canvas Painting"}
+          price={1200}
+          rating={4.5}
+        />
+        <Card
+          image={
+            "https://www.freecodecamp.org/news/content/images/size/w2000/2020/12/fcc-bg-image-2.png"
+          }
+          name={"Canvas Painting"}
+          price={1200}
+          rating={4.5}
+        />
+      </div>
+      <Footer />
+    </div>
+  );
 }
--- src/components/subscribe/Subscribe.js
+++ src/components/subscribe/Subscribe.js
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function Subscribe() {
-    return (
-        <div>
-            subscribe
-        </div>
-    )
+  return <div>subscribe</div>;
 }
