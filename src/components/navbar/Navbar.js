d9b34a538fbe9723d7c290266bfbaf628718b8c8
Abdulrafay <54246840+abdulrafayn001@users.noreply.github.com>
2022-01-18 20:01:16 +0500

    Subscribe Page Completed

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,119 +1,239 @@
-import React,{useState} from 'react';
-import logo from '../../utility/svg/logo.svg'
-import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import React, { useState } from "react";
+import logo from "../../utility/svg/logo.svg";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 export default function Navbar() {
-    const history=useHistory();
-    const [isOpen,setIsOpen] = useState(false);
-    const [selectedButton,setSelectedButton] = useState(0);
-    const toggle = () => setIsOpen(!isOpen);
+  const history = useHistory();
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedButton, setSelectedButton] = useState(0);
+  const toggle = () => setIsOpen(!isOpen);
 
-    const buttonStyle="flex w-1/4 justify-center h-full hover:text-green-light hover:bg-green-dark rounded-br-full rounded-bl-full items-center cursor-pointer"
-    const buttonStyleOnClick="flex w-1/4 justify-center h-full text-green-light bg-green-dark rounded-br-full rounded-bl-full items-center cursor-pointer"
-    console.log(selectedButton,"SELECTED BUTTON")
-    const btnStyleMobile="h-12 pr-5 pl-8 bg-green-dark text-green-lightest cursor-pointer flex items-center hover:bg-green hover:text-green-dark"
-    const btnStyleMobileOnClick = "h-12 pr-5 pl-8 bg-green text-green-dark cursor-pointer flex items-center"
-    return (
-        <nav className="bg-green-dark">
-            <div className="hidden sm:contents">
-                <div className="container flex justify-center items-center px-4 sm:px-6 lg:px-8 h-20">
-                    <div className="w-3/12 justify-start">
-                        <img className="block mx-16 h-16 rounded-full sm:mx-0 sm:flex-shrink-0 cursor-pointer" src={logo} alt="artbyhmna logo" title="ArtByHmna" onClick={()=>{history.replace('/'); setSelectedButton(0);}} />
-                    </div>
-                    <div>
-                        <div className="relative"> <input type="text" className="h-14 w-96 pr-8 pl-5 z-0 focus:shadow focus:outline-none rounded-xl hover:bg-green-lightest bg-green-light focus:bg-green-lightest text-green-dark" placeholder="Search anything..."/>
-                            <div className="absolute top-4 right-3"> <i className="fa fa-search text-green-dark z-20 hover:text-gray-light cursor-pointer"></i> </div>
-                        </div>
-                    </div>
-                    
-                    <div className="w-3/12 flex flex-row-reverse">
-                        <i className="fa fa-shopping-cart text-green-light hover:text-green-lightest fa-2x cursor-pointer" aria-hidden="true" onClick={()=>{setSelectedButton(5);history.replace('/cart')}}></i>
-                    </div>
-                </div>
+  const buttonStyle =
+    "flex w-1/4 justify-center h-full hover:text-green-light hover:bg-green-dark rounded-br-full rounded-bl-full items-center cursor-pointer";
+  const buttonStyleOnClick =
+    "flex w-1/4 justify-center h-full text-green-light bg-green-dark rounded-br-full rounded-bl-full items-center cursor-pointer";
+  console.log(selectedButton, "SELECTED BUTTON");
+  const btnStyleMobile =
+    "h-12 pr-5 pl-8 bg-green-dark text-green-lightest cursor-pointer flex items-center hover:bg-green hover:text-green-dark";
+  const btnStyleMobileOnClick =
+    "h-12 pr-5 pl-8 bg-green text-green-dark cursor-pointer flex items-center";
+  return (
+    <nav className="bg-green-dark">
+      <div className="hidden sm:contents">
+        <div className="container flex justify-center items-center px-4 sm:px-6 lg:px-8 h-20">
+          <div className="w-3/12 justify-start">
+            <img
+              className="block mx-16 h-16 rounded-full sm:mx-0 sm:flex-shrink-0 cursor-pointer"
+              src={logo}
+              alt="artbyhmna logo"
+              title="ArtByHmna"
+              onClick={() => {
+                history.replace("/");
+                setSelectedButton(0);
+              }}
+            />
+          </div>
+          <div>
+            <div className="relative">
+              {" "}
+              <input
+                type="text"
+                className="h-14 w-96 pr-8 pl-5 z-0 focus:shadow focus:outline-none rounded-xl hover:bg-green-lightest bg-green-light focus:bg-green-lightest text-green-dark"
+                placeholder="Search anything..."
+              />
+              <div className="absolute top-4 right-3">
+                {" "}
+                <i className="fa fa-search text-green-dark z-20 hover:text-gray-light cursor-pointer"></i>{" "}
+              </div>
             </div>
-            <div className="sm:justify-center sm:px-6 h-10 hidden sm:flex sm:bg-green sm:text-green-dark">
-                <div className={selectedButton===1?buttonStyleOnClick:buttonStyle} onClick={()=>{setSelectedButton(1); history.replace('/affiliate-program')}}>
-                    AFFILIATE PROGRAM
-                </div>
-                
-                <div className={selectedButton===2?buttonStyleOnClick:buttonStyle} onClick={()=>{setSelectedButton(2); history.replace('/track-order')}}>
-                    TRACK MY ORDER
-                </div>
+          </div>
 
-                <div className={selectedButton===3?buttonStyleOnClick:buttonStyle} onClick={()=>{setSelectedButton(3); history.replace('/contact')}}>
-                    CONTACT US
-                </div>
+          <div className="w-3/12 flex flex-row-reverse">
+            <i
+              className="fa fa-shopping-cart text-green-light hover:text-green-lightest fa-2x cursor-pointer"
+              aria-hidden="true"
+              onClick={() => {
+                setSelectedButton(5);
+                history.replace("/cart");
+              }}
+            ></i>
+          </div>
+        </div>
+      </div>
+      <div className="sm:justify-center sm:px-6 h-10 hidden sm:flex sm:bg-green sm:text-green-dark">
+        <div
+          className={selectedButton === 1 ? buttonStyleOnClick : buttonStyle}
+          onClick={() => {
+            setSelectedButton(1);
+            history.replace("/affiliate-program");
+          }}
+        >
+          AFFILIATE PROGRAM
+        </div>
 
-                <div className={selectedButton===4?buttonStyleOnClick:buttonStyle} onClick={()=>{setSelectedButton(4); history.replace('/subscribe')}}>
-                    SUBSCRIBE
-                </div>
+        <div
+          className={selectedButton === 2 ? buttonStyleOnClick : buttonStyle}
+          onClick={() => {
+            setSelectedButton(2);
+            history.replace("/track-order");
+          }}
+        >
+          TRACK MY ORDER
+        </div>
+
+        <div
+          className={selectedButton === 3 ? buttonStyleOnClick : buttonStyle}
+          onClick={() => {
+            setSelectedButton(3);
+            history.replace("/contact");
+          }}
+        >
+          CONTACT US
+        </div>
+
+        <div
+          className={selectedButton === 4 ? buttonStyleOnClick : buttonStyle}
+          onClick={() => {
+            setSelectedButton(4);
+            history.replace("/subscribe");
+          }}
+        >
+          SUBSCRIBE
+        </div>
+      </div>
+
+      {/* Mobile */}
+      <div className="sm:hidden">
+        <div className="flex items-center px-4 sm:px-6 lg:px-8 h-20">
+          <div className="w-1/2 flex justify-items-start cursor-pointer">
+            <img
+              className=" h-16 rounded-full sm:mx-0 "
+              src={logo}
+              alt="artbyhmna logo"
+              onClick={() => {
+                setSelectedButton(0);
+                history.replace("/");
+              }}
+            />
+          </div>
+
+          <div className="w-1/2 flex flex-row-reverse" onClick={toggle}>
+            <i
+              className="fa fa-bars text-green-light hover:text-green-lightest fa-2x cursor-pointer"
+              aria-hidden="true"
+            ></i>
+          </div>
+        </div>
+      </div>
+
+      {isOpen && (
+        <div className="sm:hidden flex flex-col justify-center">
+          <div className="relative">
+            {" "}
+            <input
+              type="text"
+              className="h-14 w-full pr-8 pl-5 z-0 focus:shadow focus:outline-none hover:bg-green-lightest bg-green-light focus:bg-green-lightest text-green-dark"
+              placeholder="Search anything..."
+            />
+            <div className="absolute top-4 right-3">
+              {" "}
+              <i className="fa fa-search text-green-dark z-20 cursor-pointer"></i>{" "}
             </div>
-
-            {/* Mobile */}
-            <div className="sm:hidden">
-                <div className="flex items-center px-4 sm:px-6 lg:px-8 h-20">
-                    <div className="w-1/2 flex justify-items-start cursor-pointer">
-                        <img className=" h-16 rounded-full sm:mx-0 " src={logo} alt="artbyhmna logo" onClick={()=>{setSelectedButton(0); history.replace('/')}} />
-                    </div>
-                    
-                    <div className="w-1/2 flex flex-row-reverse" onClick={toggle}>
-                        <i className="fa fa-bars text-green-light hover:text-green-lightest fa-2x cursor-pointer" aria-hidden="true"></i>
-                    </div>
-                </div>
+          </div>
+          <div
+            className={
+              selectedButton === 1
+                ? btnStyleMobileOnClick
+                : btnStyleMobile + " border-b border-green-light"
+            }
+            onClick={() => {
+              setSelectedButton(1);
+              history.replace("/affiliate-program");
+            }}
+          >
+            <div className="w-3/4 text-2xl h-full items-center flex">
+              <div>AFFILIATE PROGRAM</div>
             </div>
+            <div className="w-1/4 flex flex-row-reverse">
+              <i class="fa fa-share" aria-hidden="true"></i>
+            </div>
+          </div>
 
-            {isOpen && (
-                <div className="sm:hidden flex flex-col justify-center">
-                    <div className="relative"> <input type="text" className="h-14 w-full pr-8 pl-5 z-0 focus:shadow focus:outline-none hover:bg-green-lightest bg-green-light focus:bg-green-lightest text-green-dark" placeholder="Search anything..."/>
-                        <div className="absolute top-4 right-3"> <i className="fa fa-search text-green-dark z-20 cursor-pointer"></i> </div>
-                    </div>
-                    <div className={selectedButton===1?btnStyleMobileOnClick:btnStyleMobile + " border-b border-green-light"} onClick={()=>{setSelectedButton(1); history.replace('/affiliate-program')}}>
-                        <div className="w-3/4 text-2xl h-full items-center flex" >
-                            <div>AFFILIATE PROGRAM</div>
-                        </div>
-                        <div className="w-1/4 flex flex-row-reverse">
-                            <i class="fa fa-share" aria-hidden="true"></i>
-                        </div>
-                    </div>
+          <div
+            className={
+              selectedButton === 2
+                ? btnStyleMobileOnClick
+                : btnStyleMobile + " border-b border-green-light"
+            }
+            onClick={() => {
+              setSelectedButton(2);
+              history.replace("/track-order");
+            }}
+          >
+            <div className="w-3/4 text-2xl h-full items-center flex">
+              <div>TRACK MY ORDER</div>
+            </div>
+            <div className="w-1/4 flex flex-row-reverse">
+              <i class="fa fa-map-marker" aria-hidden="true"></i>
+            </div>
+          </div>
 
-                    <div className={selectedButton===2?btnStyleMobileOnClick:btnStyleMobile + " border-b border-green-light"} onClick={()=>{setSelectedButton(2);history.replace('/track-order')}}>
-                        <div className="w-3/4 text-2xl h-full items-center flex">
-                            <div>TRACK MY ORDER</div>
-                        </div>
-                        <div className="w-1/4 flex flex-row-reverse" >
-                            <i class="fa fa-map-marker" aria-hidden="true"></i>
-                        </div>
-                    </div>
+          <div
+            className={
+              selectedButton === 3
+                ? btnStyleMobileOnClick
+                : btnStyleMobile + " border-b border-green-light"
+            }
+            onClick={() => {
+              setSelectedButton(3);
+              history.replace("/contact");
+            }}
+          >
+            <div className="w-3/4 text-2xl h-full items-center flex">
+              <div>CONTACT US</div>
+            </div>
+            <div className="w-1/4 flex flex-row-reverse">
+              <i class="fa fa-mobile" aria-hidden="true"></i>
+            </div>
+          </div>
 
-                    <div className={selectedButton===3?btnStyleMobileOnClick:btnStyleMobile + " border-b border-green-light"} onClick={()=>{setSelectedButton(3);history.replace('/contact')}}>
-                        <div className="w-3/4 text-2xl h-full items-center flex" >
-                            <div>CONTACT US</div>
-                        </div>
-                        <div className="w-1/4 flex flex-row-reverse">
-                            <i class="fa fa-mobile" aria-hidden="true"></i>
-                        </div>
-                    </div>
+          <div
+            className={
+              selectedButton === 4
+                ? btnStyleMobileOnClick
+                : btnStyleMobile + " border-b border-green-light"
+            }
+            onClick={() => {
+              setSelectedButton(4);
+              history.replace("/cart");
+            }}
+          >
+            <div className="w-3/4 text-2xl h-full items-center flex">
+              <div>SUBSCRIBE</div>
+            </div>
+            <div className="w-1/4 flex flex-row-reverse">
+              <i class="fa fa-user-plus" aria-hidden="true"></i>
+            </div>
+          </div>
 
-                    <div className={selectedButton===4?btnStyleMobileOnClick:btnStyleMobile + " border-b border-green-light"} onClick={()=>{setSelectedButton(4); history.replace('/cart')}}>
-                        <div className="w-3/4 text-2xl h-full items-center flex">
-                            <div>SUBSCRIBE</div>
-                        </div>
-                        <div className="w-1/4 flex flex-row-reverse">
-                            <i class="fa fa-user-plus" aria-hidden="true"></i>
-                        </div>
-                    </div>
-
-                    <div className={selectedButton===5?btnStyleMobileOnClick:btnStyleMobile} onClick={()=>{setSelectedButton(5);history.replace('/cart')}}>
-                        <div className="w-3/4 text-2xl h-full items-center flex">
-                            <div>CART</div>
-                        </div>
-                        <div className="w-1/4 flex flex-row-reverse">
-                            <i className="fa fa-shopping-cart " aria-hidden="true"></i>
-                        </div>
-                    </div>                
-                </div>
-            )}
-            
-        </nav>    
-    )
+          <div
+            className={
+              selectedButton === 5 ? btnStyleMobileOnClick : btnStyleMobile
+            }
+            onClick={() => {
+              setSelectedButton(5);
+              history.replace("/cart");
+            }}
+          >
+            <div className="w-3/4 text-2xl h-full items-center flex">
+              <div>CART</div>
+            </div>
+            <div className="w-1/4 flex flex-row-reverse">
+              <i className="fa fa-shopping-cart " aria-hidden="true"></i>
+            </div>
+          </div>
+        </div>
+      )}
+    </nav>
+  );
 }
--- src/components/subscribe/Subscribe.js
+++ src/components/subscribe/Subscribe.js
@@ -1,5 +1,114 @@
-import React from "react";
+import React, { useState } from "react";
+import Footer from "../footer/Footer";
 
 export default function Subscribe() {
-  return <div>subscribe</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  return (
+    <>
+      <main className="h-auto">
+        <div className="relative flex items-top justify-center min-h-screen bg-green-lightest dark:bg-gray-900 sm:items-center sm:pt-0">
+          <div className="max-w-6xl mx-auto sm:px-6 lg:px-8">
+            <div className="mt-8 overflow-hidden">
+              <div className="grid grid-cols-1 md:grid-cols-2">
+                <div className="p-6 mr-2 bg-gray-100 dark:bg-gray-800 sm:rounded-lg">
+                  <h1 className="text-4xl sm:text-5xl text-green-dark dark:text-white font-extrabold tracking-tight">
+                    Subscribe
+                  </h1>
+                  <p className="text-normal text-lg sm:text-2xl font-medium text-gray-600 dark:text-gray-400 mt-2">
+                    Fill in the form to get notification about new products and
+                    discounts.
+                  </p>
+
+                  <div className="flex items-center mt-4 text-green-dark dark:text-gray-400">
+                    <i
+                      className="fa fa-whatsapp text-green-dark hover:text-green fa-2x cursor-pointer"
+                      aria-hidden="true"
+                    ></i>
+
+                    <div className="ml-4 text-md tracking-wide font-semibold w-40 text">
+                      +92 321 1392284
+                    </div>
+                  </div>
+
+                  <div className="flex items-center mt-2 text-green-dark dark:text-green">
+                    <i class="far fa-envelope text-green-dark hover:text-green fa-2x cursor-pointer"></i>
+                    <div className="ml-4 text-md tracking-wide font-semibold w-40">
+                      artbyhumna@gmail.com
+                    </div>
+                  </div>
+                </div>
+
+                <form
+                  className="p-6 flex flex-col justify-center"
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                    alert(name, email, phone);
+                  }}
+                >
+                  <div className="flex flex-col">
+                    <label for="name" className="hidden">
+                      Full Name
+                    </label>
+                    <input
+                      type="name"
+                      name="name"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                      required
+                      id="name"
+                      placeholder="Full Name*"
+                      className="w-100 mt-2 py-3 px-3 rounded-lg bg-green-light border border-green dark:border-green-dark text-green-dark font-semibold focus:border-green-dark focus:outline-none focus:bg-green-lightest"
+                    />
+                  </div>
+
+                  <div className="flex flex-col mt-2">
+                    <label for="email" className="hidden">
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      required
+                      placeholder="Email*"
+                      className="w-100 mt-2 py-3 px-3 rounded-lg bg-green-light border border-green dark:border-green-dark text-green-dark font-semibold focus:border-green-dark focus:outline-none focus:bg-green-lightest"
+                    />
+                  </div>
+
+                  <div className="flex flex-col mt-2">
+                    <label for="tel" className="hidden">
+                      Number
+                    </label>
+                    <input
+                      type="phone"
+                      name="phone"
+                      id="phone"
+                      value={phone}
+                      onChange={(e) => setPhone(e.target.value)}
+                      placeholder="Phone #"
+                      className="w-100 mt-2 py-3 px-3 rounded-lg bg-green-light border border-green dark:border-green-dark text-green-dark font-semibold focus:border-green-dark focus:outline-none focus:bg-green-lightest"
+                    />
+                  </div>
+
+                  <button
+                    type="submit"
+                    className="md:w-32 bg-green hover:text-green-lightest hover:bg-green-dark text-green-dark font-bold py-3 px-6 rounded-lg mt-3 transition ease-in-out duration-300"
+                  >
+                    Submit
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </>
+  );
 }
